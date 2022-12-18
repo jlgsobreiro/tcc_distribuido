badcb86b8c6e5826e26c3f11ac9bdeb9b26828e2
JLGS <jlgsobreiro@gmail.com>
2022-12-18 17:09:59 -0300

    teste com rede p2p

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pythonp2p
+
+import setuptools
 
 app = Flask(__name__)
+node = pythonp2p.Node()  # start the node
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    response = ''
+    if request.method == 'POST':
+        ip = request.form.get("ip", '')
+        port = request.form.get("port", 65432)
+        print(f'IP:{ip} PORT:{port}')
+        response = node.connect_to(ip, port)
+    return render_template('main.html', response=response)
 
 
 if __name__ == '__main__':
+    node.start()
     app.run(host='0.0.0.0')
