bc5cde6eadc102ffcbc83d15eb3fda0344dd9981
JLGS <jlgsobreiro@gmail.com>
2022-12-19 12:21:32 -0300

    adicionando logger

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from flask import Flask, render_template, request
 import pythonp2p
+import logging
 
 import setuptools
 
 app = Flask(__name__)
 node = pythonp2p.Node()  # start the node
+logger = logging.getLogger(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():  # put application's code here
     response = ''
+    logger.info('Aplicativo acessado')
     if request.method == 'POST':
         ip = request.form.get("ip", '')
         port = request.form.get("port", 65432)
-        print(f'IP:{ip} PORT:{port}')
-        response = node.connect_to(ip, port)
+        logger.info(f'IP:{ip} PORT:{port}')
+        response = node.connect_to(ip, int(port))
     return render_template('main.html', response=response)
 
 
