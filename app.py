c78bc3ce558d30d369317104c37ab1d318f72dd5
JLGS <jlgsobreiro@gmail.com>
2022-12-21 13:21:55 -0300

    tenta conectar a um node fixo

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
-from flask import Flask, render_template, request
-import pythonp2p
-import logging
-
-import setuptools
+from flask import Flask, render_template, request, redirect, url_for
+from pythonp2p import Node
 
 app = Flask(__name__)
-node = pythonp2p.Node()  # start the node
-logger = logging.getLogger(__name__)
+node = Node('127.0.0.1', 65432, 65433)  # start the node
 
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():  # put application's code here
     response = ''
-    logger.info('Aplicativo acessado')
+    print('Aplicativo acessado')
     if request.method == 'POST':
         ip = request.form.get("ip", '')
         port = request.form.get("port", 65432)
-        logger.info(f'IP:{ip} PORT:{port}')
+        print(f'IP:{ip} PORT:{port}')
         response = node.connect_to(ip, int(port))
     return render_template('main.html', response=response)
 
 
+@app.route('/salvar', methods=['GET', 'POST'])
+def salvar():  # put application's code here
+    node.savestate('state.json')
+    return redirect(url_for('hello_world', response='Ok'))
+
+
 if __name__ == '__main__':
     node.start()
+    node.connect_to('127.0.0.1', 65432)
     app.run(host='0.0.0.0')
